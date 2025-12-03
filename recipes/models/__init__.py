6cb46b05fcc4dbf574ae8214c2684d57d377a453
deetibabbar <deetibabbar06@gmail.com>
2025-12-03 14:56:56 +0000

    init

--- recipes/models/__init__.py
+++ recipes/models/__init__.py
@@ -2,3 +2,4 @@ from .user import *
 from recipes.models.recipes import Recipe
 from recipes.models.follow import Follow
 from recipes.models.friendRequest import FriendRequest
+from recipes.models.rating import Rating
